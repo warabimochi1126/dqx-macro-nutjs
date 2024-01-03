52faa8c12173e968ba8bdcc9e8f448e24a29123f
warabimochi <ecccompsenyou@gmail.com>
2024-01-03 17:42:15 +0900

    fix:テッセラクトで取得する文字列を一列に修正

--- tesseract-test.js
+++ tesseract-test.js
@@ -5,5 +5,7 @@ const { createWorker } = require('tesseract.js');
   const worker = await createWorker('jpn');
   const { data: { text } } = await worker.recognize('temp.png');
   console.log(text);
+  let sortedString = text.replace(/\s/g, "");
+  console.log(sortedString);
   await worker.terminate();
 })();
