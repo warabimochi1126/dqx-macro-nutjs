626a1885c182b23c5a3abddd56664dd68b68a21b
warabimochi <satomi920003@yahoo.co.jp>
2024-01-10 06:19:22 +0900

    feat:ねらいぬいのとこ後でやる

--- kawanotebukuro-macro.js
+++ kawanotebukuro-macro.js
@@ -27,7 +27,7 @@ async function isImageEqual(imgRegion, imgUrl) {
     });
 
     // 画像を2値化する
-    imgRegionSharp.threshold(100);
+    imgRegionSharp.threshold(150);
 
     // 2値化した画像のBufferを変数に詰める
     const blackWhiteImageBuffer = await imgRegionSharp.toBuffer();
@@ -60,17 +60,125 @@ async function nextStepOnCallback(callback) {
     }
 }
 
+// 乱数生成用関数
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 (async () => {
     const imgPath = "correct-img/kawanotebukuro";
     await sleep(1000);
 
+    // つかう で判定
     await nextStepOnCallback(() => isImageEqual(new Region(1125, 790, 80, 27), `${imgPath}/tukau.png`));
     await keyboard.type(Key.Enter);
 
+    // 防具をつくる で判定
     await nextStepOnCallback(() => isImageEqual(new Region(1080, 760, 145, 27), `${imgPath}/bouguwotukuru.png`));
     await keyboard.type(Key.Enter);
 
+    // さいほう針 で判定
     await nextStepOnCallback(() => isImageEqual(new Region(990, 264, 130, 30), `${imgPath}/saihoubari.png`));
     await keyboard.type(Key.Enter);
+
+    // 種類 で判定
+    await nextStepOnCallback(() => isImageEqual(new Region(624, 264, 50, 30), `${imgPath}/syurui.png`));
+    for(let i = 0; i < 3; i++) {
+        await sleep(100 + getRandomInt(0, 100));
+        await keyboard.type(Key.Down);
+    }
+    await sleep(100 + getRandomInt(0, 100));
+    await keyboard.type(Key.Enter);
+
+    // なにをつくる？ で判定
+    await nextStepOnCallback(() => isImageEqual(new Region(624, 264, 160, 30), `${imgPath}/naniwotukuru.png`));
+    await keyboard.type(Key.Enter);
+
+    // ふつうに作る で判定
+    await nextStepOnCallback(() => isImageEqual(new Region(1080, 810, 145, 36), `${imgPath}/hutuunitukuru.png`));
+    await keyboard.type(Key.Enter);
+
+
+
+
+    // 1週目
+    
+    // どうする で判定
+    await nextStepOnCallback(() => isImageEqual(new Region(624, 264, 110, 30), `${imgPath}/dousuru.png`));
+    await keyboard.type(Key.Down);
+    await sleep(100 + getRandomInt(0, 100));
+    await keyboard.type(Key.Enter);
+
+    // とくぎ で判定
+    await nextStepOnCallback(() => isImageEqual(new Region(624, 264, 70, 30), `${imgPath}/tokugi.png`));
+    await keyboard.type(Key.Right);
+    await sleep(100 + getRandomInt(0, 100));
+    await keyboard.type(Key.Up);
+    await sleep(100 + getRandomInt(0, 100));
+    await keyboard.type(Key.Enter);
+
+    // 改心の判定ロジック
+    // どうする で判定
+    while (true) {
+        await nextStepOnCallback(() => isImageEqual(new Region(624, 264, 110, 30), `${imgPath}/dousuru.png`));
+        // 現在のぬいパワーが改心×2だったら
+        await sleep(500);
+        if (isImageEqual(new Region(1065, 282, 90, 33), `${imgPath}/kaisinkakeruni.png`)) {
+            console.log("改心×2だと判定された");
+            await keyboard.type(Key.Enter);
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Down);
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Enter);
+            break;
+        } else {
+            console.log("改心×2ではないと判定された");
+            await keyboard.type(Key.Enter);
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Enter);
+        }
+    }
+
+    // ねらいぬいロジック
+    await nextStepOnCallback(() => isImageEqual(new Region(624, 264, 110, 30), `${imgPath}/dousuru.png`));
+    await sleep(500);
+    await keyboard.type(Key.Enter);
+    await sleep(100 + getRandomInt(0, 100));
+    await keyboard.type(Key.Left);
+    await sleep(100 + getRandomInt(0, 100));
+    await keyboard.type(Key.Up);
+    await sleep(100 + getRandomInt(0, 100));
+    await keyboard.type(Key.Enter);
+    await sleep(100 + getRandomInt(0, 100));
+    await keyboard.type(Key.Enter);
+    // 2週目以降
+    for(let i = 0; i < 5; i++) {
+        await nextStepOnCallback(() => isImageEqual(new Region(624, 264, 110, 30), `${imgPath}/dousuru.png`));
+        await sleep(500);
+
+        await keyboard.type(Key.Enter);
+        
+        await sleep(100 + getRandomInt(0, 100));
+        await keyboard.type(Key.Enter);
+
+        if (i == 0 && i == 1) {
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Right);
+    
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Enter);
+        } else if (i == 2) {
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Down);
+
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Enter);
+        } else {
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Left);
+
+            await sleep(100 + getRandomInt(0, 100));
+            await keyboard.type(Key.Enter);
+        }
+    }
 })();
--- test.js
+++ test.js
@@ -8,9 +8,9 @@ function sleep(ms) {
 (async () => {
   await sleep(1000);
   
-  // await mouse.move(new Point(990, 264));
+  await mouse.move(new Point(1065, 280));
 
-  await screen.captureRegion("rarara", new Region(990, 264, 130, 30));
+  await screen.captureRegion("rarara", new Region(1065, 282, 90, 33));
 
   const temp = sharp("rarara.png");
 
