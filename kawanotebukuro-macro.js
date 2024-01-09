281dfdf406906f889b53043d409fe48a422a0578
warabimochi <satomi920003@yahoo.co.jp>
2024-01-10 01:51:32 +0900

    feat:画像比較を関数化 sharpオブジェクトから高さと横幅が取れてきてないので後で修正する

--- kawanotebukuro-macro.js
+++ kawanotebukuro-macro.js
@@ -1,34 +1,100 @@
-const { keyboard, Key, screen, Region, mouse, Point, sleep } = require("@nut-tree/nut-js");
+const { keyboard, Key, screen, Region, mouse, Point, sleep, pixelWithColor } = require("@nut-tree/nut-js");
+const Pixelmatch = require("pixelmatch");
 const sharp = require("sharp");
-const { createWorker } = require("tesseract.js");
+const PNG = require('pngjs').PNG;
+const fs = require("fs");
+
+
+// 画面上の取得した範囲を二値化した画像と想定される画像を比較する関数
+async function isImageEqual(imgRegion, imgUrl) {
+    // 渡されたregionの範囲で画像のBufferを取得する
+    const RegionImage = await screen.grabRegion(imgRegion);
+
+    
+    // TODO:取得した画像の縦幅と横幅を取得する
+    console.log("----------------------------------------");
+    console.log(`RegionImage.width:${RegionImage.width}`);
+    console.log("----------------------------------------");
+    console.log(`RegionImage.height:${RegionImage.height}`);
+    console.log("----------------------------------------");
+
+    // 取得したregionの範囲をsharpオブジェクトにする
+    const imgRegionSharp = sharp(RegionImage.data, {
+        raw: {
+            width: RegionImage.width,
+            height: RegionImage.height,
+            channels: RegionImage.channels
+        }
+    });
+
+    // TODO:sharpオブジェクトになった後の画像の縦幅と横幅を取得する
+    console.log("-----------------------------------------------------");
+    console.log(`imgRegionSharp.width:${imgRegionSharp.width}`);
+    console.log("-----------------------------------------------------");
+    console.log(`imgRegionSharp.height:${imgRegionSharp.height}`);
+    console.log("-----------------------------------------------------");
+
+    // 画像を2値化する
+    imgRegionSharp.threshold(100);
+
+    // TODO:2値化した後の画像の縦幅と横幅を取得する
+    console.log("--------------------------------------------------------");
+    console.log(`imgRegionSharp.width:${imgRegionSharp.width}`);
+    console.log("--------------------------------------------------------");
+    console.log(`imgRegionSharp.height:${imgRegionSharp.height}`);
+    console.log("--------------------------------------------------------");
+
+    // 2値化した画像のBufferを変数に詰める
+    const blackWhiteImageBuffer = await imgRegionSharp.toBuffer();
+    
+    // 引数から画像のBufferを取得する
+    const correctImageBuffer = PNG.sync.read(fs.readFileSync(imgUrl)).data;
+
+    // TODO:引っ張って来た画像の縦幅と横幅を取得する
+    const temp = PNG.sync.read(fs.readFileSync(imgUrl));
+    console.log("----------------------------------------------------");
+    console.log(`imgUrl.width:${temp.width}`);
+    console.log("----------------------------------------------------");
+    console.log(`imgUrl.height:${temp.height}`);
+    console.log("----------------------------------------------------");
+
+
+    // 差分を比較する
+    const diffValue = Pixelmatch(blackWhiteImageBuffer, correctImageBuffer, null, imgRegionSharp.width, imgRegionSharp.height);
+
+    // 差分の閾値によってreturnを返す
+    if (diffValue <= 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 (async () => {
     await sleep(1000);
-    // await mouse.move(new Point(1120, 790));
-    await screen.captureRegion("temp", new Region(1125, 790, 80, 27));
+    // const tukauImage = await screen.grabRegion(new Region(1125, 790, 80, 27));
 
-    // const image = sharp("temp.png");
-    // console.log(image);
-    // image.threshold(100);
-    // await image.toFile("temp2.png");
+    // const tukauImageSharp = sharp(tukauImage.data, {
+    //     raw: {
+    //         width: tukauImage.width,
+    //         height: tukauImage.height,
+    //         channels: tukauImage.channels
+    //     }
+    // });
+    // tukauImageSharp.threshold(100);
+    // tukauImageSharp.toFile("rarara.png");
 
 
-    const tukauImage = await screen.grabRegion(new Region(1125, 790, 80, 27));
-    console.log("-------------------------------------");
-    console.log(tukauImage);
-    console.log("-------------------------------------");
-    const tukauImageSharp = sharp(tukauImage.data, {
-        raw: {
-            width: tukauImage.width,
-            height: tukauImage.height,
-            channels: tukauImage.channels
-        }
-    });
-    tukauImageSharp.threshold(100);
-    tukauImageSharp.toFile("rarara.png");
+    // const correctImage = sharp("rarara.png");
+    // const correctImageBuffer = await correctImage.toBuffer();
 
-    const worker = await createWorker("jpn");
-    const { data: { text } } = await worker.recognize("rarara.png");
-    console.log(text);
-    await worker.terminate();
+    // const correctImageBuffer = PNG.sync.read(fs.readFileSync('rarara.png'));
+
+    // console.log(await tukauImageSharp.toBuffer());
+    // console.log(correctImageBuffer.data);
+
+    // const sabun = Pixelmatch(await tukauImageSharp.toBuffer(), correctImageBuffer.data, null, tukauImage.width, tukauImage.height);
+    // console.log(sabun);
+
+    console.log(await isImageEqual(new Region(1125, 790, 80, 27), "correct-img/kawanotebukuro/tukau.png"));
 })();
